b4b92cfcc1bd8c113465a16c1fc76691875746e2
jeanettepayne <jeanetterpayne@gmail.com>
2021-02-23 18:30:01 -0500

    added links to home page, made it so only users not logged in see welcome page - if logged in will redirect to user home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
     def home
+        if !current_user
+            render 'home'
+        else
+            redirect_to user_path(current_user)
+        end
     end
 end
