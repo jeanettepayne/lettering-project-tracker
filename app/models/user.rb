26146c9430e14268c9d12ea89232c1540707d58c
jeanettepayne <jeanetterpayne@gmail.com>
2021-02-23 18:12:48 -0500

    added user validation parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
     has_secure_password
     has_many :projects
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: {in: 5..20}
+
     def full_name
         self.first_name + " " + self.last_name
     end
