3e095d0f3cef6d0bf70286a2f95868cf1d84ea4a
jeanettepayne <jeanetterpayne@gmail.com>
2021-02-27 13:52:24 -0500

    projects now nested routes under user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,7 @@ class ProjectsController < ApplicationController
 
     def new
         @project = Project.new
+        @project.user_id = params[:id]
     end
 
     def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,12 @@ class UsersController < ApplicationController
         redirect_to user_path(@user)
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to '/'
+    end
+
     private
 
     def user_params
