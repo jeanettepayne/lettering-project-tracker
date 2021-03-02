55edfd1c8dd9cc22495f9372a896d5484d9c23e0
jeanettepayne <jeanetterpayne@gmail.com>
2021-03-01 20:32:34 -0500

    added validations

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,7 +2,8 @@ class Tool < ApplicationRecord
     has_many :projects_tools
     has_many :projects, through: :projects_tools
 
-    
+    validates :brand, uniqueness: true
+    validates :name, uniqueness: true
    
     accepts_nested_attributes_for :projects_tools
 
